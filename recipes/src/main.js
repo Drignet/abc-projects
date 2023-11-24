b3a39d2f3dc2adf135e3e2d0ddb052add175f253
Drignet <odibehenry@gmail.com>
2023-11-24 20:11:32 +0100

    files update

--- recipes/src/main.js
+++ recipes/src/main.js
@@ -1,18 +1,11 @@
 const colors = [
-  "#FFFFFF",
-  "#FFB6C1",
-  "#FFDAB9",
-  "#E6E6FA",
-  "#D3D3D3",
-  "#F5F5DC",
-  "#FFFDD0",
-  "#ADD8E6",
-  "#87CEEB",
-  "#98FB98",
-  "#FF7F50",
-  "#FA8072",
-  "#FFD700",
-  "#FFFF00"
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "indigo",
+  "violet"
 ];
 
 const caption = document.querySelector(".caption");
